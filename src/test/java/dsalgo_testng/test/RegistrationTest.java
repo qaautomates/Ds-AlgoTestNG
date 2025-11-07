4869c2286422243b9b3088b40a3c85f758e86e59
Thilagavathi Amulraj <Thilagavathi.Amulraj90@gmail.com>
2025-11-07 16:20:25 -0600

    commiting tree and registration module

--- src/test/java/dsalgo_testng/test/RegistrationTest.java
+++ src/test/java/dsalgo_testng/test/RegistrationTest.java
@@ -1,5 +1,90 @@
 package dsalgo_testng.test;
 
-public class RegistrationTest {
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import baseTest.BaseTest;
+import pages.RegisterPage;
+import utilities.DriverFactory;
+
+public class RegistrationTest extends BaseTest {
+
+	ThreadLocal<RegisterPage> registerPage = new ThreadLocal<>();
+
+	@BeforeMethod
+	public void initialization() {
+		registerPage.set(new RegisterPage(DriverFactory.getDriver(), getContext().getHelper()));
+		Reporter.log("Register Page Initialized");
+
+		registerPage.get().clickRegisterLink();
+	}
+
+	@Test(groups = "launchRequired")
+	public void verify_UserName_ErrorMsg() {
+		registerPage.get().clickRegisterBtn();
+		registerPage.get().validateMessageUname();
+		Reporter.log("UserName error message verified");
+
+	}
+
+	@Test(groups = "launchRequired")
+	public void verify_Password_ErrorMsg() {
+		registerPage.get().enterUname();
+		registerPage.get().clickRegisterBtn();
+		registerPage.get().validateMessagepassword();
+		Reporter.log("Password error message verified");
+
+	}
+
+	@Test(groups = "launchRequired")
+	public void verify_CPassword_ErrorMsg() {
+		registerPage.get().enterUnamePwd();
+		registerPage.get().clickRegisterBtn();
+		registerPage.get().validateMessageCpassword();
+		Reporter.log("Confirmation password error message verified");
+
+	}
+
+	@Test(groups = "launchRequired", dataProvider = "invalid Credentials")
+	public void verify_InvalidCredentials_ErrorMsg(String userName, String passWord, String cPassword,
+			String expectedMessage) {
+		registerPage.get().enterCredentials(userName, passWord, cPassword, expectedMessage);
+		Reporter.log("Invalid credentails entered and verified");
+
+	}
+
+	@DataProvider(name = "invalid Credentials")
+	public Object[][] invalidData() {
+		Reporter.log("Invalid credentials data provider executed");
+		return new Object[][] {
+				{ "2434342", "September2025$", "September2025$", "UserName cannot be entirely numberic" },
+				{ "TestQA", "123$", "123$", "Your password must contain at least 8 characters" },
+				{ "TestQA", "test", "test", "Your password can’t be a commonly used password" },
+				{ "TestQA", "123", "123", "Your password can’t be entirely numeric." },
+				{ "TestQA", "September2025$", "Oct2025", "password_mismatch:The two password fields didn’t match." },
+
+		};
+
+	}
+
+	@Test(dataProvider = "valid Credentials", groups = "launchRequired")
+	public void validCredentailsLogin(String passWord, String cPassword, String expectedMessage) {
+		String userName = registerPage.get().generateUserName();
+		registerPage.get().enterValidCredentials(userName, passWord, cPassword, expectedMessage);
+		Reporter.log("User registered with valid credentails");
+
+	}
+
+	@DataProvider(name = "valid Credentials")
+	public Object[][] validData() {
+		Reporter.log("Valid credentails data provider executed");
+		return new Object[][] { { "September2025$", "September2025$", "New Account Created. You are logged in as " },
+
+		};
+
+	}
 }
--- src/test/java/pages/TreePage.java
+++ src/test/java/pages/TreePage.java
@@ -2,21 +2,23 @@ package pages;
 
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
 public class TreePage {
-	private static Logger logger = LogManager.getLogger();
 
-	public WebDriver driver;
-	public Helper helper;
+	private WebDriver driver;
+	private Helper helper;
 
-	@FindBy(linkText = "Sign in")
-	WebElement signInBtn;
+	@FindBy(linkText = "Try here>>>")
+	WebElement tryHereBtn;
+	@FindBy(xpath = "//button[@type='button']")
+	WebElement runBtn;
+	@FindBy(id ="output") WebElement output;
 
 	public TreePage(WebDriver driver, Helper helper) {
 		this.driver = driver;
@@ -25,51 +27,41 @@ public class TreePage {
 
 	}
 
-	public void loginToPortal() throws IOException  {
-		helper.homeGetStartedBtn();
-		signInBtn.click();
-		helper.login();
-		logger.info("User Logged in to DS algo portal for testing Tree module");
 
-		// helper.login();
-	}
-
-	public void treeGetStarted() {
-		helper.dataStructuresGetStarted("tree");
-		logger.info("Opening Tree module page using button");
-	}
-
-	public void selectTreeFromDropDown(String string) {
-		helper.selectDropDownMenu(string);
-		logger.info("Opening Tree module page using dropdown");
-	}
-
-	public void treeClickLink(String string) {
-		helper.clickLink(string);
-		logger.info("Clicking sub module" + string + " link for Tree");
+	public void treeClickLink(String link) {
+		WebElement subModElementname = driver.findElement(By.xpath("//a[text()='" + link + "']"));
+		helper.clickElement(subModElementname);
+		Reporter.log("Tree Sub Module : "+link+ "is clicked");
 	}
 
 	public void clickTreeTryEditor() {
-		helper.clickTryEditor();
-		logger.info("Clicking Try here button for Tree modules");
+		helper.clickElement(tryHereBtn);
+		Reporter.log("try here button is clicked");
 	}
 
-	public void enterTreePythonCode(String sheet, String testcase_id) throws IOException {
-		helper.enterPythonCode(helper.readFromExcel(sheet, testcase_id, "pythonCode"));
-		logger.info("Entering python code for Tree modules");
+	public void enterTreePythonCode(String sheet, String testId) throws IOException {
+		helper.enterPythonCode(helper.readFromExcel(sheet, testId, "pythonCode"));
+		Reporter.log("Python code is entered");
 	}
 
 	public void clickTreeRunBtn() {
-		helper.clickRunButton();
-		logger.info("Clicking Run button for assessment page");
+		helper.clickElement(runBtn);
+		Reporter.log("Run button is clicked");
 	}
 
 	public String getActualOutputForTree() {
-		return helper.readActualOutput();
+		String output_Message = helper.readTryEditorAlertMessage();
+		if(output_Message == null) {
+			Reporter.log("reading Actual output for tree message in Dsalgo application");
 
+			return output.getText();
+		}return output_Message;
 	}
+	
 
 	public String readExpectedOutputForTree(String sheet, String testcase_id) throws IOException {
+		Reporter.log("Reading Expected message from the excel sheet or tree");
+
 		return helper.readFromExcel(sheet, testcase_id, "Result");
 
 	}
